665531f5cec67f5e8638bde227bf8fb49699bd89
akash <akash@akashs-MacBook-Air.local>
2023-05-27 15:30:26 +0530

    game

--- src/App.js
+++ src/App.js
@@ -1,23 +1,167 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect, useRef } from "react";
+import { defaultImage } from "./assets/images";
+import ReactDOM from "react-dom";
+import DATA from "./data";
 
-function App() {
+let saveOpenImageName = [];
+const Backdrop = () => {
+  return <div className="backdrop" />;
+};
+function App(props) {
+  const [timer, setTimer] = useState(35);
+  const [score, setScore] = useState(0);
+  const [gameOver, setGameOver] = useState(false);
+  const [startStop, setStartStop] = useState(false);
+  const [storeData, setStoreData] = useState(DATA);
+  const modalContainer = useRef();
+
+  useEffect(() => {
+    let interval;
+    if (startStop) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer - 1);
+      }, 1000);
+
+      if (timer === 0) {
+        clearInterval(interval);
+        setStartStop(!startStop);
+        setGameOver(!gameOver);
+      }
+    }
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [timer, startStop]);
+
+  const gameStart = () => {
+    modalContainer.current.style.display = "none";
+    setStartStop(!startStop);
+    setStoreData(DATA);
+  };
+
+  const forImageCheck = (image) => {
+    saveOpenImageName.push(image.name);
+    if (saveOpenImageName.length === 2) {
+      let allEqual = saveOpenImageName.every(
+        (val) => val === saveOpenImageName[0]
+      );
+
+      if (!allEqual) {
+        const updateData = storeData.map((item) => {
+          if (saveOpenImageName.includes(item.name)) {
+            return {
+              ...item,
+              open: false,
+            };
+          }
+          return item;
+        });
+        if (score > 0) {
+          setScore((score) => score - 10);
+        }
+        setStoreData(updateData);
+      } else {
+        setScore((score) => score + 10);
+      }
+
+      saveOpenImageName = [];
+    }
+  };
+
+  const handleImage = (checkImage) => {
+    const updateData = storeData.map((item) => {
+      if (item.id === checkImage.id) {
+        return {
+          ...item,
+          open: true,
+        };
+      }
+      return item;
+    });
+    setStoreData(updateData);
+    setTimeout(() => {
+      forImageCheck(checkImage);
+    }, 300);
+  };
+
+  const resetGame = () => {
+    setStoreData([]);
+    setTimer(50);
+    setGameOver(false);
+    modalContainer.current.style.display = "flex";
+  };
+
+  const ModalOverlay = () => {
+    return (
+      <div className="modal_">
+        <div className="header">
+          <h2>SCORE</h2>
+        </div>
+        <div className="content">
+          <p
+            style={{
+              fontWeight: "bold",
+              fontSize: "35px",
+            }}
+          >
+            YOUR SCORE: {score}
+          </p>
+        </div>
+        <footer className="actions">
+          <button onClick={resetGame}>OKAY / RESET</button>
+        </footer>
+      </div>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {gameOver && (
+        <>
+          {ReactDOM.createPortal(
+            <Backdrop onConfirm={props.onConfirm} />,
+            document.getElementById("backdrop-root")
+          )}
+          {ReactDOM.createPortal(
+            <ModalOverlay />,
+            document.getElementById("overlay-root")
+          )}
+        </>
+      )}
+      <nav className="nav_bar">
+        <div>Timer - {timer}</div>
+        <div>Magic Cards</div>
+      </nav>
+      <div className="modalContainer" ref={modalContainer}>
+        <div className="modal">
+          <button className="btn_game" onClick={gameStart}>
+            GAME START
+          </button>
+        </div>
+      </div>
+      <div className="main_card">
+        {storeData.map((item, index) => {
+          return (
+            <div
+              className="card_child"
+              key={index}
+              onClick={() => handleImage(item)}
+            >
+              <p className="name">{item.name}</p>
+              {item.open ? (
+                <div className="pic_parent">
+                  <img className="pic" src={item.pic} alt={"pic"} />
+                </div>
+              ) : (
+                <div className="pic_parent">
+                  <img className="pic" src={defaultImage} alt={"pic"} />
+                </div>
+              )}
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/assets/images/index.js
@@ -0,0 +1,20 @@
+// import Images
+import Luffy from "./Luffy.png";
+import Sanji from "./Sanji.png";
+import Usopp from "./Usopp.png";
+import Zorro from "./zorro.png";
+import Nami from "./Nami.png";
+import Brooks from "./brooks.png";
+import onePiece from "./onepiece.png";
+
+export const images = {
+  Luffy,
+  Sanji,
+  Usopp,
+  Zorro,
+  Nami,
+  Brooks,
+  onePiece,
+};
+
+export const defaultImage = onePiece;
